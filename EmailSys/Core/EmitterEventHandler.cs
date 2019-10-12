b5bacb0c36e6dc3e6924bf06ca17b9350047c5c7
hessli <hnyxlmq@163.com>
2019-10-12 19:12:57 +0800

    x

--- EmailSys/Core/EmitterEventHandler.cs
+++ EmailSys/Core/EmitterEventHandler.cs
@@ -2,14 +2,4 @@
 namespace EmailSys.Core
 {
     public delegate void SendCompleteHandler(object sender, params SendResultEventArgs[] args);
-
-    //public delegate void EmitterSuccessEventHandler(object sender, EmitterSuccessEventArgs args);
-
-    //public delegate void EmitterSmtpErrorEventHandler(object sender,EmitterSmtpErrorEventAgs args);
-
-    //public delegate void EmitterReleasEventHandler(object sender,EmitterReleasEventArgs[] args);
-
-    //public delegate void EmitterErrorEventHandler(object sender,EmitterErrorEventArgs args);
-
-    //public delegate void EmitterArgErrorEventHandler(object sender, EmitterArgErrorEventArgs args);
 }
